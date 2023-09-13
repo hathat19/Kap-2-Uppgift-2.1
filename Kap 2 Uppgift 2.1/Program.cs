e0a5b46bd7117c23597100f6f8e55b122313b520
humy06001 <humy06001@E-PW0234KS>
2023-09-13 13:15:15 +0200

    Add project files.

---
+++ Kap 2 Uppgift 2.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Uppgift2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Frågar efter favoritbok
+            Console.WriteLine("Vad heter din favoritbok?");
+            string favBook = Console.ReadLine();
+
+            //Skriver ut namnet på boken
+            Console.WriteLine($"Din favoritbok heter {favBook}!");
+            Console.ReadKey();
+        }
+    }
+}
